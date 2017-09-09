bbbf114afdd0e70be11305842598f163da6c1a64
Alessandro Kulya <K.u.l.y.a@mail.ru>
2017-09-09 22:34:49 +0300

    Update SQL

--- sql/updates/world/2017_09_09_world_XX.sql
+++ sql/updates/world/2017_09_09_world_XX.sql
@@ -1,4 +1,5 @@
-UPDATE `creature_template` SET `ScriptName` = 'boss_adarogg' WHERE `entry` = 61408;
+UPDATE `creature_template` SET `AIName` = "", `ScriptName` = "boss_adarogg" WHERE `entry` = 61408;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=61408 AND `source_type`=0;
 DELETE FROM `spell_script_names` WHERE `spell_id` = 119405;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (119405, 'spell_inferno_charge');
