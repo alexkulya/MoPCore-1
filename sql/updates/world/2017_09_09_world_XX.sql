d8b6d8335783a26906d2854a55d60585d0516bc8
Alessandro Kulya <K.u.l.y.a@mail.ru>
2017-09-09 22:37:35 +0300

    Update SQL

--- sql/updates/world/2017_09_09_world_XX.sql
+++ sql/updates/world/2017_09_09_world_XX.sql
@@ -3,3 +3,6 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=61408 AND `source_type`=0;
 DELETE FROM `spell_script_names` WHERE `spell_id` = 119405;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (119405, 'spell_inferno_charge');
+UPDATE `creature_template` SET `AIName` = "", `ScriptName` = 'boss_koranthal' WHERE `entry` = 61412;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=61412 AND `source_type`=0;
+DELETE FROM `creature` WHERE `guid` = 989581;
